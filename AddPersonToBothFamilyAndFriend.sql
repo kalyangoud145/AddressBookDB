c5b3dfda9f38808ac154a19df2370f19454ea9fa
kalyangoud145 <kalyangoud145@gmail.com>
2020-11-20 21:38:13 +0530

    [Kalyan] added person to both family and friend

---
+++ AddPersonToBothFamilyAndFriend.sql
@@ -0,0 +1,8 @@
+--Adding a person to both friends and family
+INSERT INTO AddressBook
+VALUES
+(
+    'Bhanu','Nunna','1000 Michigan Ave.','Chicago','IL',60605,'3889233120','Nunna@gmail.com','MyBook','Family'
+)
+
+select * from AddressBook;
